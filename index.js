d2be6ace9a572a96724368b7b02702b5171fe9e1
hiro88hyo <hiro88hyo@gmail.com>
2015-02-18 17:00:47 +0900

    no message

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions)
-  google.maps.event.addListener(map, 'idle', filterPortals);
-}
-
-function filterPortals(){
-  var tim_start = Date.now();
-  var bounds = map.getBounds();
-  var tim_end = Date.now();
-  console.log("Time: " + (tim_end-tim_start)/1000 + "ms");
+    google.maps.event.addListener(map, 'idle', function(){
+      var tim_start = Date.now();
+      var bounds = map.getBounds();
+      var tim_end = Date.now();
+      console.log("Time: " + (tim_end-tim_start)/1000 + "ms");
+    });
 }
