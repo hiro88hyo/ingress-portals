42ce5935d2a152ae36b97d4ffb3e7d3e98cea7a9
hiro88hyo <hiro88hyo@gmail.com>
2015-02-18 09:42:56 +0900

    First Map Application
    Event Test
    
    pos
    
    no message
    
    Portals and Time
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message
    
    no message

---
+++ index.js
@@ -0,0 +1,48 @@
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(35.464512, 139.614334),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var markers = [];
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions)
+    google.maps.event.addListener(map, 'idle', function(){
+      var tim_start = Date.now();
+      var bounds = map.getBounds();
+
+      var minLat = bounds.getSouthWest().lat();
+      var maxLat = bounds.getNorthEast().lat();
+      var minLon = bounds.getSouthWest().lng();
+      var maxLon = bounds.getNorthEast().lng();
+
+      
+      console.log("Lat: [" + minLat +" to " + maxLat +"], Lng: [" + minLon +" to " + maxLon +"]");
+
+      markers.forEach(function(marker){
+        marker.setMap(null);
+      });
+      markers = [];
+
+      var filterd_portals = portals.filter(function(portal){
+        return (portal.lat >= minLat && portal.lat <= maxLat
+                && portal.lon >= minLon && portal.lon <= maxLon);
+      });
+
+      var iconOpt = {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 10,
+      fillColor: '#3679B9',
+      fillOpacity: 0.4,
+      strokeColor: '#3679B9',
+      strokeWeight: 2,
+      strokeOpacity: 0.8 };
+
+      filterd_portals.forEach(function(portal){
+        var marker = new google.maps.Marker({ position: new google.maps.LatLng(portal.lat, portal.lon), map: map, title: portal.name, icon: iconOpt, zIndex: 10 });
+        markers.push(marker);
+      });
+      
+      var tim_end = Date.now();
+      console.log("Portals: " + filterd_portals.length + ", Time: " + (tim_end-tim_start) + "ms");
+    });
+}
