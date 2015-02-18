8022b496dd10c3dea7ada9c1c3da7f71e88624ca
hiro88hyo <hiro88hyo@gmail.com>
2015-02-18 16:54:28 +0900

    Portals and Time

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(35.464512, 139.614334),
-    zoom: 8,
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions)
-  google.maps.event.addListener(map, 'idle', function() {
-    var bounds = map.getBounds();
-      var pos = "minLat:" + bounds.getSouthWest().lat();
-      console.log(pos);
-      //bounds.getSouthWest();
-  });
+  google.maps.event.addListener(map, 'idle', filterPortals);
+}
+
+function filterPortals(){
+  var tim_start = Data.now();
+  var bounds = map.getBounds();
+  var tim_end = Date.now();
+  console.log("Time: " + (tim_end-tim_start)/1000 + "ms";
 }
