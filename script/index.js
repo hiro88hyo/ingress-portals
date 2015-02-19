0ead4559f8bf6adba13bfe1940df5b068c3bc6a5
hiro88hyo <hiro88hyo@gmail.com>
2015-02-19 11:31:25 +0900

    content

--- script/index.js
+++ script/index.js
@@ -44,7 +44,7 @@ function initialize() {
         content += "<a href='https://www.ingress.com/intel?pll=" + portal.lat + "," + portal.lon + "'>";
         content += "intel...</a";
         var marker = new google.maps.Marker({ position: new google.maps.LatLng(portal.lat, portal.lon), map: map, title: portal.name, icon: iconOpt, zIndex: 10 });
-        var infoWindow = new google.maps.InfoWindow({ content: "<div>"+portal.name+"</div>", disableAutoPan: false, maxWidth: 640, noSupress: true });
+        var infoWindow = new google.maps.InfoWindow({ content: content, disableAutoPan: false, maxWidth: 640, noSupress: true });
         google.maps.event.addListener(marker, 'click', function(_marker, _infoWindow) { // closure
           return function(event) {
             if (openedInfoWindow) { // !!! global
