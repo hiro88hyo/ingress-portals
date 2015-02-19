f86e33a6924ba5bd0641336e6f8ccd8b08d98e5e
hiro88hyo <hiro88hyo@gmail.com>
2015-02-19 11:30:16 +0900

    link to intel

--- script/index.js
+++ script/index.js
@@ -39,6 +39,10 @@ function initialize() {
       strokeOpacity: 0.8 };
 
       filterd_portals.forEach(function(portal){
+        var content = "";
+        content += "<div>"+portal.name+"</div>";
+        content += "<a href='https://www.ingress.com/intel?pll=" + portal.lat + "," + portal.lon + "'>";
+        content += "intel...</a";
         var marker = new google.maps.Marker({ position: new google.maps.LatLng(portal.lat, portal.lon), map: map, title: portal.name, icon: iconOpt, zIndex: 10 });
         var infoWindow = new google.maps.InfoWindow({ content: "<div>"+portal.name+"</div>", disableAutoPan: false, maxWidth: 640, noSupress: true });
         google.maps.event.addListener(marker, 'click', function(_marker, _infoWindow) { // closure
