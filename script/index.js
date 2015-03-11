5e9ad55321d257f3ea6cde0a40ccd00c259aceb3
hiro88hyo <hiro88hyo@gmail.com>
2015-03-11 15:19:12 +0900

    toggle Portal

--- script/index.js
+++ script/index.js
@@ -1,4 +1,5 @@
 var isCluster = true;
+var isPortal = true;
 var map;
 var heatmap;
 
@@ -39,39 +40,41 @@ function initialize() {
     });
     markers = [];
     
-    var filterd_portals = portals.filter(function(portal){
-      return (portal.lat >= minLat && portal.lat <= maxLat
-              && portal.lon >= minLon && portal.lon <= maxLon);
-    });
-    
-    var iconOpt = {
-    path: google.maps.SymbolPath.CIRCLE,
-    scale: 6,
-    fillColor: '#3679B9',
-    fillOpacity: 0.4,
-    strokeColor: '#3679B9',
-    strokeWeight: 2,
-    strokeOpacity: 0.8 };
-    
-    filterd_portals.forEach(function(portal){
-      var content = "";
-      content += "<div>"+portal.name+"</div>";
-      content += "<a href='https://www.ingress.com/intel?pll=" + portal.lat + "," + portal.lon + "' target=_blank>";
-      content += "intel...</a>";
-      var marker = new google.maps.Marker({ position: new google.maps.LatLng(portal.lat, portal.lon), map: map, title: portal.name, icon: iconOpt, zIndex: 10 });
-      var infoWindow = new google.maps.InfoWindow({ content: content, disableAutoPan: false, maxWidth: 640, noSupress: true });
-      google.maps.event.addListener(marker, 'click', function(_marker, _infoWindow) { // closure
-        return function(event) {
-          if (openedInfoWindow) { // !!! global
-            openedInfoWindow.close();
-            openedInfoWindow = null;
-          }
-          //_infoWindow.setContent(_infoWindow.getContent().replace(/<img no_load_src=/gi, '<img src=')); // lazy img loading
-          _infoWindow.open(map, _marker);
-          openedInfoWindow = _infoWindow; };
-      }(marker, infoWindow));
-      markers.push(marker);
-    });
+    if (isPortal){
+      var filterd_portals = portals.filter(function(portal){
+        return (portal.lat >= minLat && portal.lat <= maxLat
+                && portal.lon >= minLon && portal.lon <= maxLon);
+      });
+      
+      var iconOpt = {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 6,
+      fillColor: '#3679B9',
+      fillOpacity: 0.4,
+      strokeColor: '#3679B9',
+      strokeWeight: 2,
+      strokeOpacity: 0.8 };
+      
+      filterd_portals.forEach(function(portal){
+        var content = "";
+        content += "<div>"+portal.name+"</div>";
+        content += "<a href='https://www.ingress.com/intel?pll=" + portal.lat + "," + portal.lon + "' target=_blank>";
+        content += "intel...</a>";
+        var marker = new google.maps.Marker({ position: new google.maps.LatLng(portal.lat, portal.lon), map: map, title: portal.name, icon: iconOpt, zIndex: 10 });
+        var infoWindow = new google.maps.InfoWindow({ content: content, disableAutoPan: false, maxWidth: 640, noSupress: true });
+        google.maps.event.addListener(marker, 'click', function(_marker, _infoWindow) { // closure
+          return function(event) {
+            if (openedInfoWindow) { // !!! global
+              openedInfoWindow.close();
+              openedInfoWindow = null;
+            }
+            //_infoWindow.setContent(_infoWindow.getContent().replace(/<img no_load_src=/gi, '<img src=')); // lazy img loading
+            _infoWindow.open(map, _marker);
+            openedInfoWindow = _infoWindow; };
+        }(marker, infoWindow));
+        markers.push(marker);
+      });
+    }
     if(isCluster){
       markerCluster.clearMarkers();
       markerCluster.addMarkers(markers);
@@ -93,4 +96,7 @@ function toggleCluster(){
 }
 function toggleHeatmap(){
   heatmap.setMap(heatmap.getMap() ? null : map);
+}
+function togglePortal(){
+  isPortal =isPortal?false:true;
 }
