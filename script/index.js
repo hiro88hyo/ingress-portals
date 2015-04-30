0ad5d5a870dacc9374e98c2af3f3c6f1e2fb961f
hiro88hyo <hiro88hyo@gmail.com>
2015-04-30 15:45:05 +0900

    cluster flag update

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-var isCluster = true;
+var isCluster = false;
 var isPortal = true;
 var map;
 var heatmap;
@@ -12,7 +12,7 @@ function initialize() {
   var markers = [];
   var openedInfoWindow;
   map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-  
+
 //  var attacks = [
 //    new google.maps.LatLng(35.448197,139.636295)
 //    ];
@@ -23,29 +23,29 @@ function initialize() {
   heatmap.set('radius', 50);
   heatmap.set('opacity', 0.7);
   heatmap.setMap(map);
-      
+
   google.maps.event.addListener(map, 'idle', function(){
     var tim_start = Date.now();
     var bounds = map.getBounds();
-    
+
     var minLat = bounds.getSouthWest().lat();
     var maxLat = bounds.getNorthEast().lat();
     var minLon = bounds.getSouthWest().lng();
     var maxLon = bounds.getNorthEast().lng();
-    
+
     console.log("Lat: [" + minLat +" to " + maxLat +"], Lng: [" + minLon +" to " + maxLon +"]");
-    
+
     markers.forEach(function(marker){
       marker.setMap(null);
     });
     markers = [];
-    
+
     if (isPortal){
       var filterd_portals = portals.filter(function(portal){
         return (portal.lat >= minLat && portal.lat <= maxLat
                 && portal.lon >= minLon && portal.lon <= maxLon);
       });
-      
+
       var iconOpt = {
       path: google.maps.SymbolPath.CIRCLE,
       scale: 6,
@@ -54,7 +54,7 @@ function initialize() {
       strokeColor: '#3679B9',
       strokeWeight: 2,
       strokeOpacity: 0.8 };
-      
+
       filterd_portals.forEach(function(portal){
         var content = "";
         content += "<div>"+portal.name+"</div>";
@@ -81,7 +81,7 @@ function initialize() {
     }else{
       markerCluster.clearMarkers();
     }
-    
+
     var tim_end = Date.now();
     console.log("Portals: " + filterd_portals.length + ", Time: " + (tim_end-tim_start) + "ms");
   });
